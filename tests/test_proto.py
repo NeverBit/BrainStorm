22360f9ddb76eb1c3d45b1e435b82e5b8e51a6a6
Shai Shapira <shaishapira@mail.tau.ac.il>
2020-02-27 20:07:33 +0200

    test snap slim

--- tests/test_proto.py
+++ tests/test_proto.py
@@ -22,3 +22,25 @@ def test_snapshot_ser_des_with_nones():
     s1 = proto.Snapshot(101,trans,rot,col_img,dep_img,feel)
     s2 = proto.Snapshot.fromDict(s1.toDict())
     assert repr(s1) == repr(s2)
+
+
+
+def test_snapshotslim_ser_des():
+    col_img_path = 'path/to/file'
+    dep_img_path = 'path/to/other/file'
+    trans = {'x':1.1,'y':2.2,'z':3.3}
+    rot = {'x':1.1,'y':2.2,'z':3.3,'w':4.4}
+    feel = (1.0,0.0,-1.0,1.0)
+    s1 = proto.SnapshotSlim(40,101,trans,rot,col_img_path,dep_img_path,feel)
+    s2 = proto.SnapshotSlim.fromDict(s1.toDict())
+    assert repr(s1) == repr(s2)
+
+def test_snapshotslim_ser_des_with_nones():
+    col_img_path = 'path/to/file'
+    dep_img_path = 'path/to/other/file'
+    trans = None
+    rot = None
+    feel = None
+    s1 = proto.SnapshotSlim(40,101,trans,rot,col_img_path,dep_img_path,feel)
+    s2 = proto.SnapshotSlim.fromDict(s1.toDict())
+    assert repr(s1) == repr(s2)
