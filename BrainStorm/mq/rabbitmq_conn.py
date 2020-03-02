a31b538b3e65079dde85c0a0a760c0778bc4f224
Shai Shapira <shaishapira@mail.tau.ac.il>
2020-03-02 15:48:16 +0200

    Trying to adjust to MQ topics

--- BrainStorm/mq/rabbitmq_conn.py
+++ BrainStorm/mq/rabbitmq_conn.py
@@ -1,31 +1,40 @@
 import pika
 
 class rabbitmq_conn:
-    def __init__(self,host,port,topic):
+    def __init__(self,host,port,exchange):
         self.host = host
         self.port = port
-        self.topic = topic
+        self.exchange = exchange
 
     def open(self):
         # Create connection to MQ
         params = pika.ConnectionParameters(self.host, self.port)
         connection = pika.BlockingConnection(params)
         self.channel = connection.channel()
-        self.channel.exchange_declare(exchange=self.topic, exchange_type='fanout')
+        self.channel.exchange_declare(exchange=self.exchange, exchange_type='topic')
 
-    def publish(self,msg):
+    def publish(self,msg,topic='default_topic'):
         # publishing to the entire exchange
         self.channel.basic_publish(
-            exchange = self.topic,
-            routing_key = '',
+            exchange = self.exchange,
+            routing_key = topic,
             body = msg
         )
         
-    def start_consume(self,callback):
+    def start_consume(self,callback,topics=None):
         # Creating new nameless queue in the exchange and consuming from it
         result = self.channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
-        self.channel.queue_bind(exchange=self.topic, queue=queue_name)
+
+        # If caller provided topic(s) to bind to - user them.
+        # Otherwise use '#' which consumes all topics
+        if not topics:
+            topics = ('#')
+        for binding_key in topics:
+            self.channel.queue_bind(exchange=self.topic,
+                                    queue=queue_name, 
+                                    routing_key=binding_key)
+
         self.channel.basic_consume(
             queue = queue_name,
             auto_ack = True,
--- BrainStorm/parsers.py
+++ BrainStorm/parsers.py
@@ -46,7 +46,15 @@ def run_parser_once(name,input):
     res_path.mkdir(exist_ok=True)
     context = parser_context(res_path)
     print(' @@@ Debug parser context made')
-    res = parse_func(context,snapshot)
+    parser_results = parse_func(context,snapshot)
+    # Wrap the parer results in a unified format for the server
+    saver_msg = {
+        'uid':snapshot.uid,
+        'datetime':snapshot.datetime,
+        'parser_name':name,
+        'parser_res':parser_results
+    }
+    saver_msg_json = json.dumps(saver_msg)
     print(' @@@ Debug in done parsing')
     json_snap = json.dumps(res)
     print(json_snap)
@@ -60,16 +68,23 @@ def run_parser_service(name,connection_string):
     parse_func = registered_parsers[name]
 
     # Create MQ connection towards saver
-    con_to_saver = mq.create_mq_connection(connection_string, 'save')
+    con_to_saver = mq.create_mq_connection(connection_string, 'parsers')
     con_to_saver.open()
 
     # Define parse & publish callback
     def callback(channel, method, properties, body):
         snapshot = SnapshotSlim.fromDict(json.loads(body))
         context = parser_context(Path('resources'))
-        res = parse_func(context,snapshot)
-        json_snap = json.dumps(res)
-        con_to_saver.publish(json_snap)
+        parser_results = parse_func(context,snapshot)
+        # Wrap the parer results in a unified format for the server
+        saver_msg = {
+            'uid':snapshot.uid,
+            'datetime':snapshot.datetime,
+            'parser_name':name,
+            'parser_res':parser_results
+        }
+        saver_msg_json = json.dumps(saver_msg)
+        con_to_saver.publish(saver_msg_json, topic=name)
 
     # Consume input mq
     con_to_input = mq.create_mq_connection(connection_string, 'input')
