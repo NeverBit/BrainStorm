0c908e9584c4cd9543f2e706f0ee7a8ec6bf27a2
Shai Shapira <shaishapira@mail.tau.ac.il>
2020-04-06 19:50:32 +0300

    fixes

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,3 +1,4 @@
 sudo docker build --tag bs_base -f deploy/Dockerfile.bs_base .
 sudo docker build --tag bs_server -f deploy/Dockerfile.server .
 
+sudo docker run --name bs_server_inst -p 8000:8000 bs_server
