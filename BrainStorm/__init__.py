fbe628c3c26d66133f9d9283732f5a5157505ab4
Shai Shapira <shaishapira@mail.tau.ac.il>
2020-04-07 15:49:21 +0300

    Removed class

--- BrainStorm/__init__.py
+++ BrainStorm/__init__.py
@@ -1,8 +1,9 @@
 from .client import upload_sample
-from .db_access import DbBase, Reader
+from .db_access import Reader
 from .gui import WebServer
 from .image import image
 from .mindreader import get_reader
 from .parsers import registered_parsers
+from .proto import Snapshot, SnapshotSlim
 from .server import run_server
 from .saver import Saver
